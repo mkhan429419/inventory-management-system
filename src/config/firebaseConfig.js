ba3e7cbe13d083d0b832a934bc84008cb38377c0
Maheen Akhtar Khan <116183925+mkhan429419@users.noreply.github.com>
2024-08-01 21:52:12 +0400

    firebase config updates + DB designed

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAfm8VKXcr0mWnezC2GD5ALsleaRmN4X9k",
-  authDomain: "inventorymanagement-2b663.firebaseapp.com",
-  projectId: "inventorymanagement-2b663",
-  storageBucket: "inventorymanagement-2b663.appspot.com",
-  messagingSenderId: "362145594088",
-  appId: "1:362145594088:web:8dcf2477291295a50be75d",
-  measurementId: "G-Y5G2B01FJY",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
