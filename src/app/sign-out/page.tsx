63f4cdfcf3a86c8466a1f12646b6db8483ee6de7
Maheen Akhtar Khan <116183925+mkhan429419@users.noreply.github.com>
2024-08-01 23:54:21 +0400

    deleted errors

--- src/app/sign-out/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/(auth)/sign-in/page.tsx
-import { SignIn } from "@clerk/nextjs";
-import React from "react";
-
-const SignInPage = () => {
-  return <SignIn />;
-};
-
-export default SignInPage;
